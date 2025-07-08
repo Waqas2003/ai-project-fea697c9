d0032bda0b88d5845f5ac8aedbf9e1c6885d4838
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 17:09:50 +0500

    Add src\components\ProductList.js

---
+++ src\components\ProductList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Product from './Product';
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Product product={product} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
